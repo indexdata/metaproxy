d7e08b36e1d7e76b3f14616d8dede97bdbb16c03
Adam Dickmeiss <adam@indexdata.dk>
2005-10-16 16:09:58 +0000

    Do not require net access in test test_filter_z3950_client_4

--- src/test_filter_z3950_client.cpp
+++ src/test_filter_z3950_client.cpp
@@ -1,4 +1,4 @@
-/* $Id: test_filter_z3950_client.cpp,v 1.1 2005-10-16 16:05:44 adam Exp $
+/* $Id: test_filter_z3950_client.cpp,v 1.2 2005-10-16 16:09:58 adam Exp $
    Copyright (c) 2005, Index Data.
 
 %LICENSE%
@@ -39,39 +39,37 @@ BOOST_AUTO_TEST_CASE( test_filter_z3950_client_2 )
 {
     try 
     {
-        {
-	    yp2::RouterChain router;
-
-            yp2::filter::Z3950Client zc;
-
-	    router.rule(zc);
-
-            // Create package with Z39.50 init request in it
-	    yp2::Package pack;
-
-            ODR odr = odr_createmem(ODR_ENCODE);
-            Z_APDU *apdu = zget_APDU(odr, Z_APDU_initRequest);
-
-            BOOST_CHECK(apdu);
-
-            pack.request() = apdu;
-            odr_destroy(odr);
-
-            // Put it in router
-	    pack.router(router).move(); 
-
-            // Inspect that we got Z39.50 init Response - a Z39.50 session
-            // specify a virtual host
-            yazpp_1::GDU *gdu = &pack.response();
-
-            BOOST_CHECK(pack.session().is_closed()); 
-
-            Z_GDU *z_gdu = gdu->get();
-            BOOST_CHECK(z_gdu);
-            if (z_gdu) {
-                BOOST_CHECK_EQUAL(z_gdu->which, Z_GDU_Z3950);
-                BOOST_CHECK_EQUAL(z_gdu->u.z3950->which, Z_APDU_initResponse);
-            }
+        yp2::RouterChain router;
+        
+        yp2::filter::Z3950Client zc;
+        
+        router.rule(zc);
+        
+        // Create package with Z39.50 init request in it
+        yp2::Package pack;
+        
+        ODR odr = odr_createmem(ODR_ENCODE);
+        Z_APDU *apdu = zget_APDU(odr, Z_APDU_initRequest);
+        
+        BOOST_CHECK(apdu);
+        
+        pack.request() = apdu;
+        odr_destroy(odr);
+        
+        // Put it in router
+        pack.router(router).move(); 
+        
+        // Inspect that we got Z39.50 init Response - a Z39.50 session
+        // specify a virtual host
+        yazpp_1::GDU *gdu = &pack.response();
+        
+        BOOST_CHECK(pack.session().is_closed()); 
+        
+        Z_GDU *z_gdu = gdu->get();
+        BOOST_CHECK(z_gdu);
+        if (z_gdu) {
+            BOOST_CHECK_EQUAL(z_gdu->which, Z_GDU_Z3950);
+            BOOST_CHECK_EQUAL(z_gdu->u.z3950->which, Z_APDU_initResponse);
         }
     }
     catch ( ... ) {
@@ -83,39 +81,37 @@ BOOST_AUTO_TEST_CASE( test_filter_z3950_client_3 )
 {
     try 
     {
-        {
-	    yp2::RouterChain router;
+        yp2::RouterChain router;
+        
+        yp2::filter::Z3950Client zc;
 
-            yp2::filter::Z3950Client zc;
-
-	    router.rule(zc);
-
-            // Create package with Z39.50 present request in it
-	    yp2::Package pack;
-
-            ODR odr = odr_createmem(ODR_ENCODE);
-            Z_APDU *apdu = zget_APDU(odr, Z_APDU_presentRequest);
-
-            BOOST_CHECK(apdu);
-
-            pack.request() = apdu;
-            odr_destroy(odr);
-
-            // Put it in router
-	    pack.router(router).move(); 
-
-            // Inspect that we got Z39.50 close - a Z39.50 session must start
-            // with an init !
-            yazpp_1::GDU *gdu = &pack.response();
-
-            BOOST_CHECK(pack.session().is_closed()); 
-
-            Z_GDU *z_gdu = gdu->get();
-            BOOST_CHECK(z_gdu);
-            if (z_gdu) {
-                BOOST_CHECK_EQUAL(z_gdu->which, Z_GDU_Z3950);
-                BOOST_CHECK_EQUAL(z_gdu->u.z3950->which, Z_APDU_close);
-            }
+        router.rule(zc);
+        
+        // Create package with Z39.50 present request in it
+        yp2::Package pack;
+        
+        ODR odr = odr_createmem(ODR_ENCODE);
+        Z_APDU *apdu = zget_APDU(odr, Z_APDU_presentRequest);
+        
+        BOOST_CHECK(apdu);
+        
+        pack.request() = apdu;
+        odr_destroy(odr);
+        
+        // Put it in router
+        pack.router(router).move(); 
+        
+        // Inspect that we got Z39.50 close - a Z39.50 session must start
+        // with an init !
+        yazpp_1::GDU *gdu = &pack.response();
+        
+        BOOST_CHECK(pack.session().is_closed()); 
+        
+        Z_GDU *z_gdu = gdu->get();
+        BOOST_CHECK(z_gdu);
+        if (z_gdu) {
+            BOOST_CHECK_EQUAL(z_gdu->which, Z_GDU_Z3950);
+            BOOST_CHECK_EQUAL(z_gdu->u.z3950->which, Z_APDU_close);
         }
     }
     catch ( ... ) {
@@ -127,38 +123,38 @@ BOOST_AUTO_TEST_CASE( test_filter_z3950_client_4 )
 {
     try 
     {
+        yp2::RouterChain router;
+        
+        yp2::filter::Z3950Client zc;
+        
+        router.rule(zc);
+        
+        // Create package with Z39.50 init request in it
+        yp2::Package pack;
+        
+        ODR odr = odr_createmem(ODR_ENCODE);
+        Z_APDU *apdu = zget_APDU(odr, Z_APDU_initRequest);
+        
+        const char *vhost = "localhost:9999";
+        if (vhost)
+            yaz_oi_set_string_oidval(&apdu->u.initRequest->otherInfo,
+                                     odr, VAL_PROXY, 1, vhost);
+        BOOST_CHECK(apdu);
+        
+        pack.request() = apdu;
+        odr_destroy(odr);
+        
+        // Put it in router
+        pack.router(router).move(); 
+        
+        if (pack.session().is_closed())
         {
-	    yp2::RouterChain router;
-
-            yp2::filter::Z3950Client zc;
-
-	    router.rule(zc);
-
-            // Create package with Z39.50 init request in it
-	    yp2::Package pack;
-
-            ODR odr = odr_createmem(ODR_ENCODE);
-            Z_APDU *apdu = zget_APDU(odr, Z_APDU_initRequest);
-            
-            const char *vhost = "localhost:9999";
-            if (vhost)
-                yaz_oi_set_string_oidval(&apdu->u.initRequest->otherInfo,
-                                         odr, VAL_PROXY, 1, vhost);
-
-            BOOST_CHECK(apdu);
-
-            pack.request() = apdu;
-            odr_destroy(odr);
-
-            // Put it in router
-	    pack.router(router).move(); 
-
-            // Inspect that we got Z39.50 close - a Z39.50 session must start
-            // with an init !
+            // OK, server was not up!
+        }
+        else
+        {
+            // Inspect that we got Z39.50 init response
             yazpp_1::GDU *gdu = &pack.response();
-
-            BOOST_CHECK(!pack.session().is_closed()); 
-
             Z_GDU *z_gdu = gdu->get();
             BOOST_CHECK(z_gdu);
             if (z_gdu) {
