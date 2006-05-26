c0052f39a81ac7f52a84cc006cda4cd143635de4
Adam Dickmeiss <adam@indexdata.dk>
2006-05-26 20:20:51 +0000

    Documentation now based on Docbook XML/XSLT using xsltproc + Norman Walsh's stylesheets. It means that the CVS version is no longer depending on jade/openjade or docbook-to-man scripts. PDF is no longer part of the distribution - but can optionally be made using DSSSL/jadetex or FOP.

--- buildconf.sh
+++ buildconf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.9 2006-05-03 12:03:35 mike Exp $
+# $Id: buildconf.sh,v 1.10 2006-05-26 20:20:51 adam Exp $
 
 if automake --version|head -1 |grep '1\.[4-7]'; then
     echo "automake 1.4-1.7 is active. You should use automake 1.8 or later"
@@ -73,8 +73,9 @@ Or just build the Debian packages without configuring
   dpkg-buildpackage -rfakeroot
 
 When building from a CVS checkout, you need these Debian tools:
-  autoconf, automake, libtool, gcc, docbook-utils, docbook, docbook-xml,
-  docbook-dsssl, jade, jadetex, libxslt1-dev, libyazpp1-dev,
+  autoconf, automake, libtool, gcc, g++, make,
+  xsltproc, docbook, docbook-xml, docbook-xsl,
+  libxslt1-dev, libyazpp1-dev,
   libboost-thread-dev, libboost-date-time-dev,
   libboost-program-options-dev, libboost-test-dev
 and for the image-processing needed to build the documentation:
