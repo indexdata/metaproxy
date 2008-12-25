05c78ec1f83a3c2ed086788da10b6473472b4434
Adam Dickmeiss <adam@indexdata.dk>
2008-12-25 13:21:51 +0100

    Same buildconf.sh as YAZ

--- buildconf.sh
+++ buildconf.sh
@@ -4,14 +4,12 @@ automake=automake
 aclocal=aclocal
 autoconf=autoconf
 libtoolize=libtoolize
-autoheader=autoheader
 
 test -d config || mkdir config
-if test .git; then
+if [ -d .git ]; then
     git submodule init
     git submodule update
 fi
-
 if [ "`uname -s`" = FreeBSD ]; then
     # FreeBSD intalls the various auto* tools with version numbers
     echo "Using special configuration for FreeBSD ..."
@@ -19,28 +17,30 @@ if [ "`uname -s`" = FreeBSD ]; then
     aclocal="aclocal19 -I /usr/local/share/aclocal"
     autoconf=autoconf259
     libtoolize=libtoolize15
-    autoheader=autoheader259
 fi
+
+if [ "`uname -s`" = Darwin ]; then
+    echo "Using special configuration for Darwin/MacOS ..."
+    libtoolize=glibtoolize
+fi
+
 if $automake --version|head -1 |grep '1\.[4-7]'; then
     echo "automake 1.4-1.7 is active. You should use automake 1.8 or later"
-    if test -f /etc/debian_version; then
-	echo " sudo apt-get install automake1.9"
-	echo " sudo update-alternatives --config automake"
+    if [ -f /etc/debian_version ]; then
+        echo " sudo apt-get install automake1.9"
+        echo " sudo update-alternatives --config automake"
     fi
     exit 1
 fi
 
 set -x
-
-# I am tired of underquoted warnings for Tcl macros
-$aclocal -I m4 2>&1 | grep -v aclocal/tcl.m4
-$autoheader
+$aclocal -I m4
 $libtoolize --automake --force 
 $automake --add-missing 
 $autoconf
 set -
 if [ -f config.cache ]; then
-    rm config.cache
+	rm config.cache
 fi
 
 enable_configure=false
@@ -49,32 +49,34 @@ sh_flags=""
 conf_flags=""
 case $1 in
     -d)
-    sh_flags="-g -Wall"
-    enable_configure=true
-    enable_help=false
-    shift
-    ;;
+	sh_cflags="-g -Wall -Wdeclaration-after-statement -Wstrict-prototypes"
+	sh_cxxflags="-g -Wall"
+	enable_configure=true
+	enable_help=false
+	shift
+	;;
     -c)
-    sh_flags=""
-    enable_configure=true
-    enable_help=false
-    shift
-    ;;
+	sh_cflags=""
+	sh_cxxflags=""
+	enable_configure=true
+	enable_help=false
+	shift
+	;;
 esac
 
 if $enable_configure; then
-    if test -n "$sh_flags"; then
-	CXXFLAGS="$sh_flags" ./configure --disable-shared --enable-static $*
+    if [ -n "$sh_cflags" ]; then
+	CFLAGS="$sh_cflags" CXXFLAGS="$sh_cxxflags" ./configure --disable-shared --enable-static $*
     else
 	./configure $*
     fi
 fi
 if $enable_help; then
     cat <<EOF
-    
+
 Build the Makefiles with the configure command.
   ./configure [--someoption=somevalue ...]
-  
+
 For help on options or configuring run
   ./configure --help
 
@@ -89,21 +91,30 @@ Build distribution tarball with
 Verify distribution tarball with
   make distcheck
 
+EOF
+    if [ -f /etc/debian_version ]; then
+        cat <<EOF
 Or just build the Debian packages without configuring
   dpkg-buildpackage -rfakeroot
 
-When building from a CVS checkout, you need these Debian tools:
-  autoconf, automake, libtool, gcc, g++, make,
-  xsltproc, docbook, docbook-xml, docbook-xsl, trang,
-  libxslt1-dev, libyazpp-dev,
-  libboost-thread-dev, libboost-test-dev
-and for the image-processing needed to build the documentation:
-  inkscape
+When building from Git, you need these Debian packages:
+  autoconf, automake, libtool, gcc, bison, any tcl,
+  xsltproc, docbook, docbook-xml, docbook-xsl,
+  libxslt1-dev, libssl-dev, libreadline5-dev, libwrap0-dev,
+  libpcap0.8-dev
+
+Also perhaps: libgnutls-dev libicu-dev
+
+And if you want to make a Debian package: dpkg-dev fakeroot debhelper
+(Then run "dpkg-buildpackage -rfakeroot" in this directory.)
 
 EOF
+    fi
+    if [ "`uname -s`" = FreeBSD ]; then
+        cat <<EOF
+When building from a Git, you need these FreeBSD Ports:
+  autoconf259, automake19, libtool15, bison, tcl84,
+  docbook-xsl, libxml2, libxslt, g++-4.0, make
+EOF
+    fi
 fi
-# Local Variables:
-# mode:shell-script
-# sh-indentation: 2
-# sh-basic-offset: 4
-# End:
