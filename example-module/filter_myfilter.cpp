6d79d2cc342457c25132c17d11a107c0d6930a06
Adam Dickmeiss <adam@indexdata.dk>
2010-01-14 17:09:42 +0100

    Update headers for example

--- example-module/filter_myfilter.cpp
+++ example-module/filter_myfilter.cpp
@@ -19,10 +19,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <yaz/log.h>
 #include <yaz/diagbib1.h>
 
-#include "config.hpp"
-#include "filter.hpp"
-#include "package.hpp"
-#include "util.hpp"
+#include <metaproxy/filter.hpp>
+#include <metaproxy/package.hpp>
+#include <metaproxy/util.hpp>
 
 namespace mp = metaproxy_1;
 
