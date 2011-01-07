c1d953eee6c00432493bc364da6284704ccd9cc2
Adam Dickmeiss <adam@indexdata.dk>
2011-01-07 11:32:48 +0100

    Happy new year

--- src/filter_dl.cpp
+++ src/filter_dl.cpp
@@ -1,5 +1,5 @@
 /* This file is part of Metaproxy.
-   Copyright (C) 2005-2010 Index Data
+   Copyright (C) 2005-2011 Index Data
 
 Metaproxy is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
