indexdata/metaproxy

commit db015fa75ee1f475040dba0512fca094e3a54d9d

Avoid using unix path as "database name" for virt_db
Fix split_zurl to use cs_get_host_args which considers
unix sockets as well.

src/util.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <metaproxy/util.hpp>
 
 #include <yaz/odr.h>
+#include <yaz/comstack.h>
 #include <yaz/pquery.h>
 #include <yaz/otherinfo.h>
 #include <yaz/querytowrbuf.h>
@@ -406,14 +407,15 @@ void mp_util::split_zurl(std::string zurl, std::string &host,
                          std::list<std::string> &db)
 {
     const char *zurl_cstr = zurl.c_str();
-    const char *sep = strchr(zurl_cstr, '/');
-    
-    if (sep)
-    {
-        host = std::string(zurl_cstr, sep - zurl_cstr);
+    const char *args = 0;
+    cs_get_host_args(zurl_cstr, &args);
 
-        const char *cp1 = sep+1;
-        while(1)
+    if (args && *args)
+    { 
+        host = std::string(zurl_cstr, args - zurl_cstr);
+
+        const char *cp1 = args + 1;
+        while (1)
         {
             const char *cp2 = strchr(cp1, '+');
             if (cp2)
@@ -427,9 +429,7 @@ void mp_util::split_zurl(std::string zurl, std::string &host,
         }
     }
     else
-    {
         host = zurl;
-    }
 }
 
 bool mp_util::set_databases_from_zurl(

commit c82f33874743548147f485752d0441a0691624ce

Fix split_zurl bug (missing char after /).
Bug introduced in db015fa75ee1f475040dba0512fc (version 1.3.40)

src/util.cpp
@@ -414,7 +414,7 @@ void mp_util::split_zurl(std::string zurl, std::string &host,
     { 
         host = std::string(zurl_cstr, args - zurl_cstr);
 
-        const char *cp1 = args + 1;
+        const char *cp1 = args;
         while (1)
         {
             const char *cp2 = strchr(cp1, '+');