b02df3fd0849c5222081013420c18f949c55f9c5
Adam Dickmeiss <adam@indexdata.dk>
2014-01-02 20:17:51 +0100

    Happy new year
    Omit copyright year range from most source files. We don't want to
    update them every year anymore.

--- src/html_parser.cpp
+++ src/html_parser.cpp
@@ -1,5 +1,5 @@
 /* This file is part of Metaproxy.
-   Copyright (C) 2005-2013 Index Data
+   Copyright (C) Index Data
 
 Metaproxy is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
